ccf914b4aa31f287831c383c5c2db5a0c4e74c6a
Samuel Skybjak <samuel.skybjak@student.tuke.sk>
2025-07-09 10:54:55 +0200

    feat: implement TypeORM database connection

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 
 import express from 'express';
 import * as path from 'path';
+import 'dotenv/config';
+import { ServerController } from './controllers/ServerController';
 
-const app = express();
+const server = new ServerController();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+server.app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
-});
-
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+server.startServer();
