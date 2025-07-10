f3647b3789679a0f4eb6f0049de5a7d285bcc8af
Samuel Skybjak <samuel.skybjak@student.tuke.sk>
2025-07-10 12:07:24 +0200

    feat: extend services to return user name, and recent articles

--- apps/api/src/services/ArticleServices.ts
+++ apps/api/src/services/ArticleServices.ts
@@ -1,20 +1,45 @@
-import { Article } from 'libs/database/src/lib/entities/article.entity';
 import { AppDataSource } from 'libs/database/src/lib/data-source';
+import { Article } from 'libs/database/src/lib/entities/article.entity';
+import { Not } from 'typeorm';
 
 export class ArticleService {
-  async getArticleById(userId: number, articleId: number): Promise<Article | null> {
-    
+  async getArticleById(
+    userId: number,
+    articleId: number
+  ): Promise<Article | null> {
     try {
       const articleRepository = AppDataSource.getRepository(Article);
-      const article = await articleRepository.findOne({
+      return await articleRepository.findOne({
         where: {
           id: articleId,
           user_id: userId,
         },
       });
-      return article || null;
     } catch (error) {
       console.error('Error fetching article by ID:', error);
+      throw error;
+    }
+  }
+
+  async getRecentArticlesByUser(
+    userId: number,
+    excludeArticleId: number
+  ): Promise<Article[]> {
+    try {
+      const articleRepository = AppDataSource.getRepository(Article);
+      const articles = await articleRepository.find({
+        where: {
+          user_id: userId,
+          id: Not(excludeArticleId),
+        },
+        order: {
+          published_at: 'DESC',
+        },
+        take: 5,
+      });
+      return articles;
+    } catch (error) {
+      console.error('Error fetching recent articles:', error);
       throw error;
     }
   }
