b54c63cd56d3f1061c6143ae3d3f5b74fe7e70a6
Samuel Skybjak <samuel.skybjak@student.tuke.sk>
2025-07-10 13:22:15 +0200

    feat: Add new columns to article table via migration

---
+++ libs/database/src/lib/migrations/1752146391297-AddMissingColumnsToArticle.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AddMissingColumnsToArticle1752146391297 implements MigrationInterface {
+   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumns('article', [
+      new TableColumn({
+        name: 'perex',
+        type: 'text',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'is_published',
+        type: 'boolean',
+        default: false,
+      }),
+      new TableColumn({
+        name: 'created_at',
+        type: 'timestamp',
+        default: 'CURRENT_TIMESTAMP',
+      }),
+      new TableColumn({
+        name: 'updated_at',
+        type: 'timestamp',
+        default: 'CURRENT_TIMESTAMP',
+        onUpdate: 'CURRENT_TIMESTAMP',
+      }),
+    ]);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('article', 'perex');
+    await queryRunner.dropColumn('article', 'is_published');
+    await queryRunner.dropColumn('article', 'created_at');
+    await queryRunner.dropColumn('article', 'updated_at');
+  }
+}
